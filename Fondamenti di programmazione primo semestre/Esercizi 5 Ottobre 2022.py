22de0aaafc6bf1e3643ffb4faf56edebc9b8e6b8
FedVlogger17 <66442720+FedVlogger17@users.noreply.github.com>
2022-10-08 17:18:24 +0200

    Commit forse corresto Esercizi 5 Ottobre 2022

--- Fondamenti di programmazione primo semestre/Esercizi 5 Ottobre 2022.py
+++ Fondamenti di programmazione primo semestre/Esercizi 5 Ottobre 2022.py
@@ -1,1 +1,20 @@
+import math
+#calcoli
+#1
+print(f"secondi presenti in 42minuti e 42 secondi: {(42*60)+42} secondi")
 
+#2
+print(f"numero di miglia che ci sono in 10 chilometri: {10*0.621371} miglia")
+
+#3
+print("Corridore che corre una gara di 10 chilometri in 42 minuti e 42 secondi")
+print(f"tempo per miglio in minuti e secondi: {int((((42*60)+42) / (10*0.621371) / 60))} minuti e {int((((42*60)+42) /  (10*0.621371)%60))} secondi")
+
+#4
+print(f"volume di una sfera di raggio 5: {(5/3) * math.pi * (5**3)}")
+
+#5
+print(f"costo totale di 60 copie: {(((24.95 / 100 * 40) + 0.75) * 59) + ((24.95 / 100 * 40) + 3)}")
+
+#6
+# calcolare ora in cui torniamo a casa in base al ritmo (?)
