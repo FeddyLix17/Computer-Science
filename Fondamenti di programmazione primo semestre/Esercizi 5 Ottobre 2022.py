04d1fbf6ca0569f163bfa95abb954a2e24e12301
FedVlogger17 <FedVlogger17@protonmail.com>
2022-10-15 17:40:48 +0200

    Update Esercizi 5 Ottobre 2022.py
    prima bozza esericizi finiti

--- Fondamenti di programmazione primo semestre/Esercizi 5 Ottobre 2022.py
+++ Fondamenti di programmazione primo semestre/Esercizi 5 Ottobre 2022.py
@@ -1,4 +1,6 @@
 import math
+from unittest import result
+
 #calcoli
 #1
 print(f"secondi presenti in 42minuti e 42 secondi: {(42*60)+42} secondi")
@@ -33,3 +35,61 @@ print(f"la rappresentazione finale di {stringa5caratteri} in decimale è {int(in
 #3
 stringa5caratteri = input("inserisci la tua stringa da 5 caratteri rappresentante un numero decimale: ")
 print(f"la rappresentazione finale di {stringa5caratteri} in decimale (con la virgola) è {float(stringa5caratteri)}")
+
+
+#funzioni
+#1
+def cubo_numero_virgola_mobile() -> int:
+    numeroo = float(input("inserisci un numero in virgola mobile: "))
+    return numeroo**3
+#2
+def first_equation() -> int:
+    a, b, c = input("inserisci 3 numeri separati da uno spazio: ").split()
+    x1 = -b + math.sqrt( b**2 - 4*a*c) / 2*a
+    x2 = -b - math.sqrt( b**2 - 4*a*c) / 2*a
+    return max(x1, x2) # return x1,x2 
+
+#3
+def second_equation() -> int:
+    a, b, c = input("inserisci 3 numeri separati da uno spazio: ").split()
+    x1 = -b + math.sqrt(b**2 - 4*a*c) / 2*a
+    x2 = -b - math.sqrt(b**2 - 4*a*c) / 2*a
+    return x1, x2
+
+#4
+def sum_even_minus_diff_odd() ->int:
+    nums = []
+    result = 0
+    for x in range(5):
+        nums+=input(f"inserisci il numero {x + 1}: ")
+    for x in nums:
+        if x % 2 == 0:
+            result+=x
+        else:
+            result-=x
+
+#5
+def sum_value_if_valid() -> int:
+    a, b, c = input("inserisci 3 numeri separati da uno spazio: ").split()
+    if (a >= 0 and a <= 30) and (b >= 0 and b <= 30) and (c >= 0 and c <= 30):
+        return a+b+c
+    else:
+        return -1
+
+
+#6
+def check_date() -> bool:
+    d, m, y = input("inserisci giorno mese e anno separati da uno spazio: ").split()
+    if (d >= 0 and d <= 31) and (m >= 1 and m <= 12) and (y >= 0 and y <= 2022):
+        return True
+    else:
+        return False
+
+
+#7
+def fn_str_strip(string: str) -> str:
+    return " ".join(string.split())
+
+#8
+def saluto(name: str) -> str:
+    return f"Ciao {name} Buona giornata!"
