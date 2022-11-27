c20dbcd3ccdccdf110a7cc8ceb0bef9d2e63410e
FedVlogger17 <FedVlogger17@protonmail.com>
2022-11-27 02:08:30 +0100

    update homework6

--- Fondamenti di programmazione primo semestre/HomeWork6 (obbligatorio)/troppissimosium.py
+++ Fondamenti di programmazione primo semestre/HomeWork6 (obbligatorio)/troppissimosium.py
@@ -17,15 +17,9 @@ def draw_pixel(img, x, y, colore):
 def generate_snake(start_img: str, position: list[int, int], commands: str, out_img: str) -> int:
     img, commands, snakelen, nextmove, all_positions = images.load(start_img), commands.split(), 1, 0, [position]
     moveset = {'N': (0, -1), 'S': (0, 1), 'E': (1, 0), 'W': (-1, 0),'NE': (1, -1), 'NW': (-1, -1), 'SE': (1, 1), 'SW': (-1, 1)}
-    draw_pixel(img, position[0], position[1], (0, 255, 0))
-    #all_positions.append(position)
-    command = commands[nextmove]
-    position = [position[0] + moveset[command][0], position[1] + moveset[command][1]]
-    #position = [position[0] + moveset[command][0], position[1] + moveset[command][1]]
-    #nextmove += 1
-    all_positions.append(position)
-    while nextmove < len(commands) and img[position[1] + moveset[commands[nextmove]][1]][position[0] + moveset[commands[nextmove]][0]] != (0, 255, 0) and img[position[1] + moveset[commands[nextmove]][1]][position[0] + moveset[commands[nextmove]][0]] != (255, 0, 0):
-        time.sleep(0.1)
+    while nextmove < len(commands) and position not in all_positions[-snakelen:-1] and position != (0, 255, 0): 
+        #time.sleep(0.1)
+        input()
         images.save(img, out_img)
         command = commands[nextmove]
         draw_pixel(img, all_positions[-snakelen][0], all_positions[-snakelen][1], (128, 128, 128))
@@ -40,7 +34,6 @@ def generate_snake(start_img: str, position: list[int, int], commands: str, out_
             position[1] = 0
         if img[position[1]][position[0]] == (255, 128, 0):
             snakelen += 1
-            draw_pixel(img, all_positions[-snakelen][0], all_positions[-snakelen][1], (0, 255, 0))
         draw_pixel(img, position[0], position[1], (0, 255, 0))
         all_positions.append(position)
         nextmove += 1
@@ -50,10 +43,7 @@ def generate_snake(start_img: str, position: list[int, int], commands: str, out_
     if img[all_positions[-1][1]][all_positions[-1][0]] == (255, 0, 0):
         all_positions.remove(all_positions[-1])
     nextmove = len(all_positions) - snakelen
-    while nextmove < len(all_positions):
-        draw_pixel(img, all_positions[nextmove][0], all_positions[nextmove][1], (0, 255, 0))
-        nextmove += 1
-    images.save(img, out_img)
+    #images.save(img, out_img)
     return snakelen
 
 generate_snake(start_img, position, commands, out_img)
