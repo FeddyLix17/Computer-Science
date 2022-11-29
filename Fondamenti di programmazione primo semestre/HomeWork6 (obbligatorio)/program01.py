c00f160ee25a6be33e335dda593275e3e9fb8a0f
FedVlogger17 <FedVlogger17@protonmail.com>
2022-11-29 12:59:00 +0100

    update homework 6
    ancora questo diagonal cross che è davero una spina nel fianco

--- Fondamenti di programmazione primo semestre/HomeWork6 (obbligatorio)/program01.py
+++ Fondamenti di programmazione primo semestre/HomeWork6 (obbligatorio)/program01.py
@@ -64,11 +64,15 @@ NOTA: non importate o usate altre librerie
 
 import images
 
+
 def check_diagonal_cross(all_positions, position, nextmove, command, moveset, img):
-    diagonals = {'NE': (1, 1), 'NW': (1, -1), 'SE': (-1, 1), 'SW': (-1, -1)}
-    if img[all_positions[-1][1]][all_positions[-1][1] + diagonals[command][1]] == (0, 255, 0) and img[all_positions[-1][0]][all_positions[-1][0] + diagonals[command][0]] == (0, 255, 0):
+    diagonals = {'NE': (-1, 1), 'NW': (-1, -1), 'SE': (-1, 1), 'SW': (-1, -1)}
+    print(f"current command: {command}")
+    if img[all_positions[-1][1] + diagonals[command][0]][all_positions[-1][0]] == (0, 255, 0) and img[all_positions[-1][1]][all_positions[-1][0] + diagonals[command][1]] == (0, 255, 0):
+        img[position[1]][position[0]] = (0, 255, 0)
         return False
     return True
+
 
 def check_bordeless(position, img):
     checked = 0
@@ -85,34 +89,40 @@ def check_bordeless(position, img):
         position[1] = 0
         checked = 1
     return position, checked
-        
+
 
 def generate_snake(start_img: str, position: list[int, int], commands: str, out_img: str) -> int:
     img, commands, snakelen, nextmove, all_positions = images.load(start_img), commands.split(), 1, 0, [position]
     moveset = {'N': (0, -1), 'S': (0, 1), 'E': (1, 0), 'W': (-1, 0),'NE': (1, -1), 'NW': (-1, -1), 'SE': (1, 1), 'SW': (-1, 1)}
-    diagonals = {'NE': ('N', 'E'), 'NW': ('N', 'W'), 'SE': ('S', 'E'), 'SW': ('S', 'W')}
+    diagonals = {'NE': (-1, 1), 'NW': (-1, -1), 'SE': (-1, 1), 'SW': (-1, -1)}
     img[position[1]][position[0]] = (128, 128, 128)
     command = commands[nextmove]
     position = [position[0] + moveset[command][0], position[1] + moveset[command][1]]
     nextmove += 1
     all_positions.append(position)
     while nextmove < len(commands):
-        #time.sleep(0.1)
+        #time.sleep(0.2)
         #input()
-        if img[position[1]][position[0]] == (0, 255, 0) or img[position[1]][position[0]] == (255, 0, 0): break
-        command = commands[nextmove]    
+        if img[position[1]][position[0]] == (0, 255, 0) or img[position[1]][position[0]] == (255, 0, 0):
+            break
+        command = commands[nextmove]
+        if command in diagonals.keys() and supercheck == 0:
+            if not check_diagonal_cross(all_positions, position, nextmove, command, moveset, img):
+                break
         img[all_positions[-snakelen - 1][1]][all_positions[-snakelen - 1][0]] = (128, 128, 128)
         img[position[1]][position[0]] = (0, 255, 0)
         position = [position[0] + moveset[command][0], position[1] + moveset[command][1]]
         position, supercheck = check_bordeless(position, img)
-        if img[position[1]][position[0]] == (255, 128, 0): snakelen += 1
-        if command in diagonals.keys() and supercheck == 0:
-            if not check_diagonal_cross(all_positions, position, nextmove, command, moveset, img): break
+        print(position)
+        supercheck = check_bordeless(position, img)
+        if img[position[1]][position[0]] == (255, 128, 0):
+            snakelen += 1
         all_positions.append(position)
         images.save(img, out_img)
         nextmove += 1
-    if nextmove == len(commands):
-        img[all_positions[-snakelen - 1][1]][all_positions[-snakelen - 1][0]] = (128, 128, 128)
+    if img[position[1]][position[0]] == (0, 0, 0):
+    #    img[all_positions[-snakelen - 1][1]][all_positions[-snakelen - 1][0]] = (128, 128, 128)
         img[position[1]][position[0]] = (0, 255, 0)
+        img[all_positions[-snakelen - 1][1]][all_positions[-snakelen - 1][0]] = (128, 128, 128)
     images.save(img, out_img)
     return snakelen
