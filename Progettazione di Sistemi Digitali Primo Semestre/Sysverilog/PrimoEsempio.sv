6be8eeb32979f435d141db0a2b780405de84ad84
FedVlogger17 <FedVlogger17@protonmail.com>
2023-02-04 20:09:31 +0100

    general

---
+++ Progettazione di Sistemi Digitali Primo Semestre/Sysverilog/PrimoEsempio.sv
@@ -0,0 +1,4 @@
+module example(input logic a, b, c,
+output logic y);
+assign y = ~a & ~b & ~c | a & ~b & ~c | a & ~b & c;
+endmodule
--- Progettazione di Sistemi Digitali Primo Semestre/Sysverilog/PrimoEsempio.v
+++
@@ -1,6 +0,0 @@
-module circuit (
-    input  logic a, b c
-    output logic y
-);
-  assign y = ~a & ~b & ~c | a & ~b & ~c | a & ~b & c;
-endmodule
