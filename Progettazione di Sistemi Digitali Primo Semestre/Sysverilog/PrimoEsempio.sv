99b65cba047d61164390347c28d4dcecc175c565
FedVlogger17 <FedVlogger17@protonmail.com>
2023-02-05 13:03:52 +0100

    Update PrimoEsempio.sv

--- Progettazione di Sistemi Digitali Primo Semestre/Sysverilog/PrimoEsempio.sv
+++ Progettazione di Sistemi Digitali Primo Semestre/Sysverilog/PrimoEsempio.sv
@@ -1,14 +1,8 @@
-module fulladder(
-    input logic[3:0] a, b,
-    output logic [6:0] y);
-// { } indicano sempre un vettore di bit
-// se un numero è presente davanti a {},
-// allora quel vettore viene ripetuto per N
-assign y = {a[2:1], {3{b[0]}}, a[0], 6'b100_010};
-// se y è un segnale a 12-bit, l'istruzione qua sopra produce:
-//y = { a[2] a[1] b[0] b[0] b[0] a[0] 1 0 0 0 1 0 }
-// inoltre, possiamo modificare anche
-// un singolo bit di un vettore
-//assign y[0] = 1’b1;
-//assign y[1] = 1’b0;
+module r_flop_async(input logic clk,
+input logic reset,
+input logic [3:0] d,
+output logic [3:0] q);
+always_ff @(posedge clk, posedge reset)
+if(reset) q <= 4'b0;
+else q <= d;
 endmodule
