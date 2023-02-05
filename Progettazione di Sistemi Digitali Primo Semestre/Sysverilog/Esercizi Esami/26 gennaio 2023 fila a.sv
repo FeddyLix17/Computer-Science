bbdb01675f10abf9ba86b49e31078b06b9f54dd6
FedVlogger17 <FedVlogger17@protonmail.com>
2023-02-05 22:16:12 +0100

    ordinamento file sysverilog nelle relative cartelle

--- Progettazione di Sistemi Digitali Primo Semestre/Sysverilog/Esercizi Esami/26 gennaio 2023 fila a.sv
+++ Progettazione di Sistemi Digitali Primo Semestre/Sysverilog/Esercizi Esami/26 gennaio 2023 fila a.sv
@@ -2,8 +2,10 @@ module risoluzione_esercizio(
     input logic clk, a,
     output logic[1:0] y
 );
+    logic net;
 always_ff @(posedge clk ) begin 
     y[0] <= ~a;
-    y[1] <= a;
+    net <= a;
+    y[1] <= net;
 end
 endmodule
