b66b3d1039c368980dbdd54e1968a44bf9a2450b
FedVlogger17 <FedVlogger17@protonmail.com>
2023-03-02 23:13:42 +0100

    repo update
    upload capitolo3 esercizio6

---
+++ Primo Anno/Secondo Semestre/Metodologie di Programmazione/Esercizi/Esercizi Capitolo 3/Esercizio_6/README.md
@@ -0,0 +1,9 @@
+# <p align=center> Capitolo3 Esercizio6 </p>
+
+Data una sequenza di tre numeri a virgola mobile con precisione doppia 
+letti da input, verificare: <br>
+- il caso in cui la sequenza fornita sia strettamente crescente
+- il caso in cui la sequenza fornita sia strettamente decrescente
+- la non appartenenza ai due casi precedenti
+
+Per ogni casistica, stampare l’opportuno messaggio a video.
---
+++ Primo Anno/Secondo Semestre/Metodologie di Programmazione/Esercizi/Esercizi Capitolo 3/Esercizio_6/src/Esercizio6/Solution.java
@@ -0,0 +1,33 @@
+package Esercizio6;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /*  ogni numero sarà dichiarato come double in modo 
+            da poter rispettare la richiesta per la quale i
+            3 numeri debbano essere a virgola mobile con 
+            doppia precisione 
+                                */
+
+        System.out.print("Inserire il primo numero: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Inserire il secondo numero: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Inserire il terzo numero: ");
+        double num3 = scanner.nextDouble();
+
+        if (num1 <= num2 && num2 <= num3) {
+            System.out.println("La sequenza è strettamente crescente.");
+        } else if (num1 >= num2 && num2 >= num3) {
+            System.out.println("La sequenza è strettamente decrescente.");
+        } else {
+            System.out.println("I numeri non appartengono ai casi precedenti.");
+        }
+
+        scanner.close();
+    }
+}
