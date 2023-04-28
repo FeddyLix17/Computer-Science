981a26f2aa827cac99cb24c2d7320673bb0737f3
FedVlogger17 <FedVlogger17@protonmail.com>
2023-04-28 19:54:51 +0200

    Update program01.asm

--- Primo Anno/Secondo Semestre/Architettura degli Elaboratori/Esame Mips 10 Giugno 2022/program01.asm
+++ Primo Anno/Secondo Semestre/Architettura degli Elaboratori/Esame Mips 10 Giugno 2022/program01.asm
@@ -1,8 +1,8 @@
 ##########################################
 # INSERIRE I PROPRI DATI QUI
-# Nome:
-# Cognome:
-# Matricola:
+# Nome: Valerio
+# Cognome:  Fontana
+# Matricola:    2046790
 ##########################################
 
 # NON MODIFICARE QUESTA PARTE
@@ -20,5 +20,50 @@ main:
 
 ##########################################
 ## INSERIRE IL CODICE QUI
+    li $t0, -1                          # $t0 = indice sequenza di caratteri
+    li $v0, 0                           # $v0 = numero di zeri presenti nella sequenza di caratteri
+    li $v1, 0                           # $v1 = numero di uni presenti nella sequenza di caratteri
+    jal contaOccorrenze                 # salto a contaOccorrenze
     
+    # una volta calcolati il numero di zeri e uni presenti nella sequenza di caratteri,
+    # come richiesto dalla traccia, li stampo in 2 righe separate
 
+    # stampo il numero di zeri (contenuto in $v0)
+    move $a0, $v0
+    li $v0, 1
+    syscall
+    
+    # vado a capo
+    li $v0, 11
+    li $a0, '\n'
+    syscall
+    
+    # e infine stampo il numero di uni (contenuto in $v1)
+    li $v0, 1
+    move $a0, $v1
+    syscall
+
+    # termino il programma
+    li $v0, 10
+    syscall
+
+contaOccorrenze:
+    addi $t0, $t0, 1                    # $t0 = $t0 + 1
+    lb $a0 buffer($t0)                  # $a0 = buffer[$t0]
+
+    sub $a0, $a0, '0'                   # $a0 = valore numerico del carattere contenuto in $a0/buffer[$t0]
+    beqz $a0, NumeroUgualeAZero         # se $a0 == 0 allora salta a NumeroUgualeAZero
+    beq $a0, 1, NumeroUgualeAUno        # se $a0 == 1 allora salta a NumeroUgualeAUno
+    bltz $a0, EndLoop                # se $a0 < 0 allora salta a EndProgram
+    j contaOccorrenze
+
+NumeroUgualeAZero:
+    addi $v0, $v0, 1                    # $v0 = $v0 + 1
+    j contaOccorrenze
+
+NumeroUgualeAUno:
+    addi $v1, $v1, 1                    # $v1 = $v1 + 1
+    j contaOccorrenze
+
+EndLoop:
+    jr $ra                              # ritorno all'istruzione successiva a jal contaOccorrenze
